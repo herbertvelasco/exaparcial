1fd225701b762bef63451b11a6012e2c5e2dade4
HerbertVelasco <havm14@gmail.com>
2022-10-26 16:05:01 -0500

    version 0.02

--- lib/pages/home.pages.dart
+++ lib/pages/home.pages.dart
@@ -4,19 +4,22 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter',
+      title: 'Pagina 1',
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            SizedBox(
+              height: 80.0,
+            ),
             CircleAvatar(
               radius: 70.0,
               backgroundColor: Colors.white,
               backgroundImage: AssetImage("assets/images/icopersona.png"),
             ),
             SizedBox(
-              height: 30.0,
+              height: 80.0,
             ),
             Divider(
               thickness: 0.70,
@@ -40,6 +43,9 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
+            SizedBox(
+              height: 20.0,
+            ),
             Card(
               color: Colors.white,
               margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
@@ -48,11 +54,11 @@ class HomePage extends StatelessWidget {
                 title: Text("Password"),
                 subtitle: Text("***********"),
                 leading: Icon(
-                  Icons.mail,
+                  Icons.lock_outline,
                   color: Colors.indigo,
                 ),
                 trailing: Icon(
-                  Icons.check_circle_outline,
+                  Icons.remove_red_eye_outlined,
                   color: Colors.indigo,
                 ),
               ),
@@ -68,7 +74,7 @@ class HomePage extends StatelessWidget {
                   backgroundColor: Color(0xFF001d3d).withOpacity(0.8),
                 ),
                 onPressed: () {},
-                icon: Icon(Icons.play_arrow),
+                icon: Icon(Icons.done),
                 label: Text(
                   'Login',
                   style: TextStyle(
@@ -77,6 +83,38 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
               ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 30.0,
+                ),
+                Text(
+                  "Signup",
+                  //altura.toInt().toString(),
+                  style: TextStyle(
+                    //fontFamily: 'Manrope',
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF001d3d).withOpacity(0.8),
+                  ),
+                ),
+                SizedBox(
+                  width: 290.0,
+                ),
+                Text(
+                  "Forgot Password?",
+                  style: TextStyle(
+                    //fontFamily: 'Manrope',
+                    fontSize: 14.0,
+                    fontWeight: FontWeight.w400,
+                    color: Color(0xFF001d3d).withOpacity(0.8),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
